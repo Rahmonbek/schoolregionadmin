84e753af6973c0170ea94e695ef6332a656fc0e7
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-08-22 13:03:32 +0500

    t

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Main from './pages/Main'
 import Verify from './pages/Verify'
 import Login from './pages/Login'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 export default class App extends Component {
   render() {
     return (
@@ -16,7 +17,7 @@ export default class App extends Component {
 <Verify/>
         </Route>
         
-        <Route exact path="/main">
+        <Route path="/main">
 <Main/>
         </Route>
       </Switch>

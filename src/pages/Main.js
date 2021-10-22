ca12c1a365ade858de6eb7ff14e87dff871b288d
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-10-22 15:46:46 +0500

    update

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,36 +5,41 @@ import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
 import style from "../css/Navbar.module.css";
 import Yangilik from "./Yangilik";
 import Tadbirlar from "./Tadbirlar";
-import { getSchools,getRegion } from "../host/Config";
+import { getSchools, getRegion } from "../host/Config";
 import Rahbariyat from "./Rahbariyat";
 import GLOBAL from "../host/Global";
 import Parol from "./Parol";
-import Alochilar from './Alochilar'
-import Togaraklar from './Togaraklar'
-import Yutuqlar from './Yutuqlar'
-import { UserOutlined, BookOutlined, RocketOutlined, BellOutlined } from "@ant-design/icons";
-import {FaSchool} from 'react-icons/fa'
+import Alochilar from "./Alochilar";
+import Togaraklar from "./Togaraklar";
+import Yutuqlar from "./Yutuqlar";
+import {
+  UserOutlined,
+  BookOutlined,
+  RocketOutlined,
+  BellOutlined,
+} from "@ant-design/icons";
+import { FaSchool } from "react-icons/fa";
 
 const { Header, Content, Footer, Sider } = Layout;
 
 export default class Main extends Component {
   state = {
-    oneId:null,
+    oneId: null,
     collapsed: false,
     maktab: [],
     maktabId: 0,
     current: "rahbariyat",
-    maktab1:null,
-    index:null,
-    region:[],
+    maktab1: null,
+    index: null,
+    region: [],
   };
   getSchoolsAll = () => {
-    getSchools().then((res) => this.setState({ maktab: res.data,
-      oneId:res.data[0].id
-    }));
+    getSchools().then((res) =>
+      this.setState({ maktab: res.data, oneId: res.data[0].id })
+    );
   };
   getRegions = () => {
-    getRegion().then((res) => this.setState({ region: res.data}));
+    getRegion().then((res) => this.setState({ region: res.data }));
   };
   onCollapse = (collapsed) => {
     this.setState({ collapsed });
@@ -42,9 +47,8 @@ export default class Main extends Component {
   getId = (val) => {
     this.setState({
       maktabId: val,
-      maktab1:val[0].id,
+      maktab1: val[0].id,
     });
-
   };
   handleClick = (e) => {
     console.log("click ", e);
@@ -52,46 +56,84 @@ export default class Main extends Component {
   };
 
   componentDidMount() {
-    this.getRegions()
-    this.getSchoolsAll()  
-    console.log(GLOBAL.id)
+    this.getRegions();
+    this.getSchoolsAll();
+    console.log(GLOBAL.id);
   }
   render() {
     return GLOBAL.id !== null ? (
       <div>
         <BrowserRouter>
-        <Layout style={{ minHeight: "100vh" }}>
-          <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-            <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={["0"]} mode="inline">
+          <Layout style={{ minHeight: "100vh" }}>
+            <Sider
+              collapsible
+              collapsed={this.state.collapsed}
+              onCollapse={this.onCollapse}
+            >
+              <div className="logo" />
+              <Menu theme="dark" defaultSelectedKeys={["0"]} mode="inline">
+                <Menu.Item
+                  onClick={() => this.getId(0)}
+                  key="-2"
+                  icon={<PieChartOutlined />}
+                >
+                  <Link
+                    style={{ textDecoration: "none", color: "white" }}
+                    to={`/${window.location.href.slice(
+                      window.location.href.indexOf("main"),
+                      window.location.href.lastIndexOf("/")
+                    )}/all`}
+                  >
+                    Hammasi
+                  </Link>
+                </Menu.Item>
 
-              <Menu.Item onClick={() => this.getId(0)} key="-2" icon={<PieChartOutlined />}>
-<Link style={{textDecoration:'none', color:'white'}}  to={`/${window.location.href.slice(window.location.href.indexOf("main"), window.location.href.lastIndexOf("/"))}/all`}>Hammasi</Link>
-              </Menu.Item>
+                {this.state.maktab.map((item, key) => {
+                  return (
+                    <Menu.Item
+                      onClick={() => this.getId(`${item.school_number}`)}
+                      key={key}
+                      icon={<FaSchool />}
+                    >
+                      <Link
+                        style={{ textDecoration: "none", color: "white" }}
+                        to={`/${window.location.href.slice(
+                          window.location.href.indexOf("main"),
+                          window.location.href.lastIndexOf("/")
+                        )}/${item.id}`}
+                      >
+                        {item.school_number}-maktab
+                      </Link>
+                    </Menu.Item>
+                  );
+                })}
+                <Menu.Item key="-1" icon={<PieChartOutlined />}>
+                  <Link
+                    style={{ textDecoration: "none", color: "white" }}
+                    to={`/main/parol`}
+                  >
+                    Parol o'zgartirish
+                  </Link>
+                </Menu.Item>
+              </Menu>
+            </Sider>
+            <Layout className="site-layout">
+              <Header
+                className="site-layout-background"
+                style={{ padding: 0 }}
+              />
 
-
-              {this.state.maktab.map((item, key) => {
-                return (
-                  <Menu.Item onClick={() => this.getId(`${item.school_number}`)} key={key} icon={<FaSchool />}>
-                    <Link style={{textDecoration:'none',color:'white'}} to={`/${window.location.href.slice(window.location.href.indexOf("main"), window.location.href.lastIndexOf("/"))}/${item.id}`}>{item.school_number}-maktab</Link>
-                  </Menu.Item>
-                );
-              })}
-              <Menu.Item key="-1" icon={<PieChartOutlined />}>
-<Link  style={{textDecoration:'none', color:'white'}} to={`/main/parol`}>Parol o'zgartirish</Link>
-              </Menu.Item>
-
-              
-
-
-            </Menu>
-          </Sider>
-          <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }} />
-
-            <Content style={{ margin: "0 16px" }}>
-              <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
-              <div className="site-layout-background" style={{ padding: 24, height: "540", overflowY: "auto", overflowX: "hidden" }}>
+              <Content style={{ margin: "0 16px" }}>
+                <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
+                <div
+                  className="site-layout-background"
+                  style={{
+                    padding: 24,
+                    minHeight: "540px",
+                    overflowY: "auto",
+                    overflowX: "hidden",
+                  }}
+                >
                   <Switch>
                     <Route exact path="/main/rahbariyat/:id">
                       <Rahbariyat />
@@ -115,63 +157,93 @@ export default class Main extends Component {
                       <Yutuqlar />
                     </Route>
                   </Switch>
-
-                <Footer style={{ textAlign: "center" }}>
-                  Tizim IT Tower kompaniyasi tomonidan tayyorlandi.
-                  <br /> Murojat uchun:{" "}
-                  <a style={{ textDecoration: "none", color: "black" }} href="tel:+998999349707">
-                    +998-99-934-97-07
-                  </a>
-                </Footer>
-              </div>
-                <div className={style.maktab}>
-                  {
-                    this.state.region.map(item=>{
-                      return(
-                        (GLOBAL.id===item.admin)?(
-                          <h3>{item.region_name}</h3>
-                        ):''
-                      )
-                    })
-                  }
                 </div>
-              <nav className={style.nvb}>
-            <Menu  defaultSelectedKeys={['rahbariyat']} onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-              <Menu.Item key="rahbariyat" icon={<UserOutlined />}>
-                <Link exact to={`/main/rahbariyat/${this.state.oneId}`} style={{ textDecoration: "none" }}>
-                  Rahbariyat
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="yangiliklar" icon={<BookOutlined />}>
-                <Link style={{ textDecoration: "none" }} exact to={`/main/yangiliklar/${this.state.oneId}`}>
-                  Yangiliklar
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="tadbirlar" icon={<BellOutlined />}>
-                <Link exact to={`/main/tadbirlar/${this.state.oneId}`} style={{ textDecoration: "none" }}>
-                  Tadbirlar
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="togaraklar" icon={<BellOutlined />}>
-                <Link exact to={`/main/togaraklar/${this.state.oneId}`} style={{ textDecoration: "none" }}>
-                  To'garaklar
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="yutuqlar" icon={<BellOutlined />}>
-                <Link exact to={`/main/yutuqlar/${this.state.oneId}`} style={{ textDecoration: "none" }}>
-                  Yutuqlar
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="alochilar" icon={<RocketOutlined />}>
-                <Link refresh="true" exactexact to={`/main/alochilar/${this.state.oneId}`} style={{ textDecoration: "none" }}>
-                  Alochilar
-                </Link>
-              </Menu.Item>
-            </Menu>
-        </nav>
-            </Content>
+                <div className={style.maktab}>
+                  {this.state.region.map((item) => {
+                    return GLOBAL.id === item.admin ? (
+                      <h3>{item.region_name}</h3>
+                    ) : (
+                      ""
+                    );
+                  })}
+                </div>
+                <nav className={style.nvb}>
+                  <Menu
+                    defaultSelectedKeys={["rahbariyat"]}
+                    onClick={this.handleClick}
+                    selectedKeys={[this.state.current]}
+                    mode="horizontal"
+                  >
+                    <Menu.Item key="rahbariyat" icon={<UserOutlined />}>
+                      <Link
+                        exact
+                        to={`/main/rahbariyat/${this.state.oneId}`}
+                        style={{ textDecoration: "none" }}
+                      >
+                        Rahbariyat
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item key="yangiliklar" icon={<BookOutlined />}>
+                      <Link
+                        style={{ textDecoration: "none" }}
+                        exact
+                        to={`/main/yangiliklar/${this.state.oneId}`}
+                      >
+                        Yangiliklar
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item key="tadbirlar" icon={<BellOutlined />}>
+                      <Link
+                        exact
+                        to={`/main/tadbirlar/${this.state.oneId}`}
+                        style={{ textDecoration: "none" }}
+                      >
+                        Tadbirlar
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item key="togaraklar" icon={<BellOutlined />}>
+                      <Link
+                        exact
+                        to={`/main/togaraklar/${this.state.oneId}`}
+                        style={{ textDecoration: "none" }}
+                      >
+                        To'garaklar
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item key="yutuqlar" icon={<BellOutlined />}>
+                      <Link
+                        exact
+                        to={`/main/yutuqlar/${this.state.oneId}`}
+                        style={{ textDecoration: "none" }}
+                      >
+                        Yutuqlar
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item key="alochilar" icon={<RocketOutlined />}>
+                      <Link
+                        refresh="true"
+                        exactexact
+                        to={`/main/alochilar/${this.state.oneId}`}
+                        style={{ textDecoration: "none" }}
+                      >
+                        Alochilar
+                      </Link>
+                    </Menu.Item>
+                  </Menu>
+                </nav>
+              </Content>
+              <Footer style={{ textAlign: "center" }}>
+                Tizim IT Tower kompaniyasi tomonidan tayyorlandi.
+                <br /> Murojat uchun:{" "}
+                <a
+                  style={{ textDecoration: "none", color: "black" }}
+                  href="tel:+998999349707"
+                >
+                  +998-99-934-97-07
+                </a>
+              </Footer>
+            </Layout>
           </Layout>
-        </Layout>
         </BrowserRouter>
       </div>
     ) : (

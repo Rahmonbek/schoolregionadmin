0a8a2828773c4848507ff82ed3e5e897df947cf0
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-10-23 11:54:25 +0500

    sdv

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -63,7 +63,6 @@ export default class Main extends Component {
   render() {
     return GLOBAL.id !== null ? (
       <div>
-<<<<<<< HEAD
         <BrowserRouter>
         <Layout style={{ minHeight: "100vh" }}>
           <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
@@ -77,51 +76,9 @@ export default class Main extends Component {
                 return (
                   <Menu.Item onClick={() => this.getId(`${item.school_number}`)} key={key} icon={<PieChartOutlined />}>
                     <Link to={`/${window.location.href.slice(window.location.href.indexOf("main"), window.location.href.lastIndexOf("/"))}/${item.id}`}>{item.school_number}-maktab</Link>
-=======
-        {this.state.loader === true ? (
-          <Loader />
-        ) : (
-          <BrowserRouter>
-            <Layout style={{ minHeight: "100vh" }}>
-              <Sider
-                collapsible
-                collapsed={this.state.collapsed}
-                onCollapse={this.onCollapse}
-              >
-                <div className="logo" />
-                <Menu theme="dark" defaultSelectedKeys={["0"]} mode="inline">
-                  {this.state.maktab.map((item, key) => {
-                    return (
-                      <Menu.Item
-                        onClick={() => this.getId(`${item.school_number}`)}
-                        key={key}
-                        icon={<PieChartOutlined />}
-                      >
-                        <Link
-                          to={`/${window.location.href.slice(
-                            window.location.href.indexOf("main"),
-                            window.location.href.lastIndexOf("/")
-                          )}/${item.id}`}
-                        >
-                          {item.school_number}-maktab
-                        </Link>
-                      </Menu.Item>
-                    );
-                  })}
-                  <Menu.Item
-                    onClick={() => this.getId(0)}
-                    key="-1"
-                    icon={<PieChartOutlined />}
-                  >
-                    <Link
-                      style={{ textDecoration: "none", color: "white" }}
-                      to={`/main/parol`}
-                    >
-                      Parol o'zgartirish
-                    </Link>
->>>>>>> c6dbf0c1326553c844464ea336fdf2eb195d232f
                   </Menu.Item>
-                </Menu>
+                )})}
+</Menu>
               </Sider>
               <Layout className="site-layout">
                 <Header
@@ -129,7 +86,6 @@ export default class Main extends Component {
                   style={{ padding: 0 }}
                 />
 
-<<<<<<< HEAD
             <Content style={{ margin: "0 16px" }}>
               <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
               <div className="site-layout-background" style={{ padding: 24, height: "540", overflowY: "auto", overflowX: "hidden" }}>
@@ -206,124 +162,6 @@ export default class Main extends Component {
           </Layout>
         </Layout>
         </BrowserRouter>
-=======
-                <Content style={{ margin: "0 16px" }}>
-                  <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
-                  <div
-                    className="site-layout-background"
-                    style={{
-                      padding: 24,
-                      height: "540",
-                      overflowY: "auto",
-                      overflowX: "hidden",
-                    }}
-                  >
-                    <Switch>
-                      <Route exact path="/main/rahbariyat/:id">
-                        <Rahbariyat />
-                      </Route>
-                      <Route exact path="/main/yangiliklar/:id">
-                        <Yangilik />
-                      </Route>
-                      <Route exact path="/main/tadbirlar/:id">
-                        <Tadbirlar />
-                      </Route>
-                      <Route exact path="/main/alochilar/:id">
-                        <Alochilar />
-                      </Route>
-                      <Route exact path="/main/parol">
-                        <Parol />
-                      </Route>
-                      <Route exact path="/main/togaraklar/:id">
-                        <Togaraklar />
-                      </Route>
-                      <Route exact path="/main/yutuqlar/:id">
-                        <Yutuqlar />
-                      </Route>
-                    </Switch>
-
-                    <Footer style={{ textAlign: "center" }}>
-                      Tizim IT Tower kompaniyasi tomonidan tayyorlandi.
-                      <br /> Murojat uchun:{" "}
-                      <a
-                        style={{ textDecoration: "none", color: "black" }}
-                        href="tel:+998999349707"
-                      >
-                        +998-99-934-97-07
-                      </a>
-                    </Footer>
-                  </div>
-                  <div className={style.maktab}></div>
-                  <nav className={style.nvb}>
-                    <Menu
-                      defaultSelectedKeys={["rahbariyat"]}
-                      onClick={this.handleClick}
-                      selectedKeys={[this.state.current]}
-                      mode="horizontal"
-                    >
-                      <Menu.Item key="rahbariyat" icon={<UserOutlined />}>
-                        <Link
-                          exact
-                          to={`/main/rahbariyat/${this.state.oneId}`}
-                          style={{ textDecoration: "none" }}
-                        >
-                          Rahbariyat
-                        </Link>
-                      </Menu.Item>
-                      <Menu.Item key="yangiliklar" icon={<BookOutlined />}>
-                        <Link
-                          style={{ textDecoration: "none" }}
-                          exact
-                          to={`/main/yangiliklar/${this.state.oneId}`}
-                        >
-                          Yangiliklar
-                        </Link>
-                      </Menu.Item>
-                      <Menu.Item key="tadbirlar" icon={<BellOutlined />}>
-                        <Link
-                          exact
-                          to={`/main/tadbirlar/${this.state.oneId}`}
-                          style={{ textDecoration: "none" }}
-                        >
-                          Tadbirlar
-                        </Link>
-                      </Menu.Item>
-                      <Menu.Item key="togaraklar" icon={<BellOutlined />}>
-                        <Link
-                          exact
-                          to={`/main/togaraklar/${this.state.oneId}`}
-                          style={{ textDecoration: "none" }}
-                        >
-                          To'garaklar
-                        </Link>
-                      </Menu.Item>
-                      <Menu.Item key="yutuqlar" icon={<BellOutlined />}>
-                        <Link
-                          exact
-                          to={`/main/tadbirlar/${this.state.oneId}`}
-                          style={{ textDecoration: "none" }}
-                        >
-                          Yutuqlar
-                        </Link>
-                      </Menu.Item>
-                      <Menu.Item key="alochilar" icon={<RocketOutlined />}>
-                        <Link
-                          refresh="true"
-                          exactexact
-                          to={`/main/alochilar/${this.state.oneId}`}
-                          style={{ textDecoration: "none" }}
-                        >
-                          Alochilar
-                        </Link>
-                      </Menu.Item>
-                    </Menu>
-                  </nav>
-                </Content>
-              </Layout>
-            </Layout>
-          </BrowserRouter>
-        )}
->>>>>>> c6dbf0c1326553c844464ea336fdf2eb195d232f
       </div>
     ) : (
       <Redirect to="/" />

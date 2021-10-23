1cf25915482ec9d00d2509084aa2c085f054be0a
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-10-23 17:10:29 +0500

    Update Yutuqlar.js

--- src/pages/Yutuqlar.js
+++ src/pages/Yutuqlar.js
@@ -1,46 +1,53 @@
 import React, { Component } from "react";
-import { Button, Card, Col, Container, Modal, Row } from "react-bootstrap";
+import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 import { getNew, getNews } from "../host/Config";
 import GLOBAL from "../host/Global";
-import { FaHistory,FaUserGraduate } from "react-icons/fa";
+import { FaHistory, FaUserGraduate } from "react-icons/fa";
 import { HiLocationMarker } from "react-icons/hi";
 import { message } from "antd";
-import { getSchools ,getAchievment,getPupil} from "../host/Config";
-import {IoMdTrophy} from 'react-icons/io'
+import { getSchools, getAchievment, getPupil } from "../host/Config";
+import { IoMdTrophy } from "react-icons/io";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions } from "@mui/material";
 import Loader from "./Loader";
 export default class Yutuqlar extends Component {
   state = {
-   loader:true,
-    pupil:[],
-    maktab:[],
+    loader: true,
+    pupil: [],
+    maktab: [],
     datas: [],
     data: {},
     show: false,
     id: window.location.href.slice(window.location.href.lastIndexOf("/") + 1),
   };
   getAchievments = () => {
-      getAchievment()
-        .then((res) => this.setState({ datas: res.data }))
-        .catch(() => message.error("Ma'lumot yuklanmadi"));
+    getAchievment()
+      .then((res) => this.setState({ datas: res.data }))
+      .catch(() => message.error("Ma'lumot yuklanmadi"));
   };
   getSchoolsAll = () => {
-    getSchools().then((res) => this.getSchool(res.data))
-    .catch(() => console.log("Ma'lumot yuklanmadi 2"));
+    getSchools()
+      .then((res) => this.getSchool(res.data))
+      .catch(() => console.log("Ma'lumot yuklanmadi 2"));
   };
-  getSchool=(val)=>{
+  getSchool = (val) => {
     this.setState({
-      maktab:val
-    })
-  }
+      maktab: val,
+    });
+  };
   getPupils = () => {
-    getPupil().then((res) => this.getPupilI(res.data))
-    .catch(() => console.log("Ma'lumot yuklanmadi 2"));
+    getPupil()
+      .then((res) => this.getPupilI(res.data))
+      .catch(() => console.log("Ma'lumot yuklanmadi 2"));
   };
-  getPupilI=(val)=>{
+  getPupilI = (val) => {
     this.setState({
-      pupil:val
-    })
-  }
+      pupil: val,
+    });
+  };
   showModal = (id) => {
     this.setState({ show: true, data: this.state.datas[id] });
   };
@@ -48,9 +55,9 @@ export default class Yutuqlar extends Component {
     this.setState({ show: false, data: {} });
   };
   componentDidMount() {
-      this.getPupils()
+    this.getPupils();
     this.getAchievments();
-    this.getSchoolsAll()
+    this.getSchoolsAll();
     setInterval(() => {
       this.setState({
         loader: false,
@@ -59,70 +66,195 @@ export default class Yutuqlar extends Component {
   }
   render() {
     return (
-      <div>{this.state.loader===true?(<Loader/>):(
-        <Container fluid>
-          <Row>
-            {this.state.datas !== []
-              ? this.state.datas.map((item, key) => {
-                  return(
-                  this.state.maktab.map(item2=>{
-                    return(
-                     this.state.pupil.map(item3=>{
-                         return(
-                    
-                                (parseInt(item2.id)===parseInt(item.school))?(
-                                    (parseInt(item.pupils)===parseInt(item3.id))?(
-                                    (parseInt(window.location.href.slice(window.location.href.lastIndexOf("/") + 1)) === parseInt(item.school)) ? (
-                                      <Col lg={3} md={4} sm={6} xs={12}>
-                                        <Card style={{ margin: "10px auto", borderRadius: "7px", boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px", height: "500px" }}>
-                                          <Card.Img variant="top" src={item.image} style={{ width: "100%", height: "250px" }} />
-                                          {/* <Card.Img variant="top" src="https://picsum.photos/50" /> */}
-                                          <Card.Body>
-                                            <h6 style={{ margin: "5px 0px 20px 0px", fontSize: "16px", borderBottom: "1px solid #ccc", height: "50px" }}>
-                                              <b>{item.competition}</b>
-                                            </h6>
-                  
-                                            <div style={{ margin: "10px 0px", fontSize: "15px" }}>
-                                              <span style={{ marginRight: "10px" }}>
-                                                <HiLocationMarker/>
-                                              </span>
-                                              <span>{item2.school_number}-maktab</span>
-                                            </div>
-                                            <div style={{ margin: "10px 0px", fontSize: "15px" }}>
-                                              <span style={{ marginRight: "10px" }}>
-                                                <IoMdTrophy/>
-                                                </span>
-                                                <span>{item.result}</span>       
-                                            </div>
-                                            <div style={{ margin: "10px 0px", fontSize: "15px" }}>
-                                              <span style={{ marginRight: "10px" }}>
-                                                <FaUserGraduate />
-                                                </span>
-                                                <span>{item3.full_name}</span>
-                                            </div>
-                                            <Button onClick={() => this.showModal(key)} style={{ fontSize: "12px" }}>
-                                              Batafsil
-                                            </Button>
-                                          </Card.Body>
-                                        </Card>
-                                      </Col>
-                                    ) : ''
-                                  ):""
-                             ):''
-                         )
-                     })
-                    )
+      <div>
+        {this.state.loader === true ? (
+          <Loader />
+        ) : (
+          <Container fluid>
+            <Row>
+              {this.state.datas !== []
+                ? this.state.datas.map((item, key) => {
+                    return this.state.maktab.map((item2) => {
+                      return this.state.pupil.map((item3) => {
+                        return parseInt(item2.id) === parseInt(item.school) ? (
+                          parseInt(item.pupils) === parseInt(item3.id) ? (
+                            parseInt(
+                              window.location.href.slice(
+                                window.location.href.lastIndexOf("/") + 1
+                              )
+                            ) === parseInt(item.school) ? (
+                              <Col lg={3} md={4} sm={6} xs={12}>
+                                <Card
+                                  style={{ margin: "5px", minHeight: "500px" }}
+                                >
+                                  <CardActionArea>
+                                    <CardMedia
+                                      component="img"
+                                      height="300"
+                                      image={item.image}
+                                      alt="green iguana"
+                                    />
+                                    <CardContent>
+                                      <Typography gutterBottom variant="h7">
+                                        {item.title}
+                                      </Typography>
+                                      <Typography
+                                        variant="body2"
+                                        color="text.secondary"
+                                      >
+                                        <h6
+                                          style={{
+                                            margin: "5px 0px 20px 0px",
+                                            fontSize: "16px",
+                                            borderBottom: "1px solid #ccc",
+                                            height: "50px",
+                                          }}
+                                        >
+                                          <b>{item.competition}</b>
+                                        </h6>
+
+                                        <div
+                                          style={{
+                                            margin: "10px 0px",
+                                            fontSize: "15px",
+                                          }}
+                                        >
+                                          <span style={{ marginRight: "10px" }}>
+                                            <HiLocationMarker />
+                                          </span>
+                                          <span>
+                                            {item2.school_number}-maktab
+                                          </span>
+                                        </div>
+                                        <div
+                                          style={{
+                                            margin: "10px 0px",
+                                            fontSize: "15px",
+                                          }}
+                                        >
+                                          <span style={{ marginRight: "10px" }}>
+                                            <IoMdTrophy />
+                                          </span>
+                                          <span>{item.result}</span>
+                                        </div>
+                                        <div
+                                          style={{
+                                            margin: "10px 0px",
+                                            fontSize: "15px",
+                                          }}
+                                        >
+                                          <span style={{ marginRight: "10px" }}>
+                                            <FaUserGraduate />
+                                          </span>
+                                          <span>{item3.full_name}</span>
+                                        </div>
+                                      </Typography>
+                                    </CardContent>
+                                  </CardActionArea>
+                                  <CardActions>
+                                    <Button
+                                      onClick={() => this.showModal(key)}
+                                      style={{ fontSize: "12px" }}
+                                    >
+                                      Batafsil
+                                    </Button>
+                                  </CardActions>
+                                </Card>
+
+                                {/* <Card
+                                  style={{
+                                    margin: "10px auto",
+                                    borderRadius: "7px",
+                                    boxShadow:
+                                      "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+                                    minHeight: "500px",
+                                  }}
+                                >
+                                  <Card.Img
+                                    variant="top"
+                                    src={item.image}
+                                    style={{ width: "100%", height: "250px" }}
+                                  />
+                                  <Card.Body>
+                                    <h6
+                                      style={{
+                                        margin: "5px 0px 20px 0px",
+                                        fontSize: "16px",
+                                        borderBottom: "1px solid #ccc",
+                                        height: "50px",
+                                      }}
+                                    >
+                                      <b>{item.competition}</b>
+                                    </h6>
+
+                                    <div
+                                      style={{
+                                        margin: "10px 0px",
+                                        fontSize: "15px",
+                                      }}
+                                    >
+                                      <span style={{ marginRight: "10px" }}>
+                                        <HiLocationMarker />
+                                      </span>
+                                      <span>{item2.school_number}-maktab</span>
+                                    </div>
+                                    <div
+                                      style={{
+                                        margin: "10px 0px",
+                                        fontSize: "15px",
+                                      }}
+                                    >
+                                      <span style={{ marginRight: "10px" }}>
+                                        <IoMdTrophy />
+                                      </span>
+                                      <span>{item.result}</span>
+                                    </div>
+                                    <div
+                                      style={{
+                                        margin: "10px 0px",
+                                        fontSize: "15px",
+                                      }}
+                                    >
+                                      <span style={{ marginRight: "10px" }}>
+                                        <FaUserGraduate />
+                                      </span>
+                                      <span>{item3.full_name}</span>
+                                    </div>
+                                    <Button
+                                      onClick={() => this.showModal(key)}
+                                      style={{ fontSize: "12px" }}
+                                    >
+                                      Batafsil
+                                    </Button>
+                                  </Card.Body>
+                                </Card> */}
+                              </Col>
+                            ) : (
+                              ""
+                            )
+                          ) : (
+                            ""
+                          )
+                        ) : (
+                          ""
+                        );
+                      });
+                    });
                   })
-                    
-                  )
-                })
-              : ""}
-           
-          </Row>
-        </Container>)}
-        <Modal show={this.state.show} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+                : ""}
+            </Row>
+          </Container>
+        )}
+        <Modal
+          show={this.state.show}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
           <Modal.Header>
-            <Modal.Title id="contained-modal-title-vcenter">Yutuq: {this.state.data.competition}</Modal.Title>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Yutuq: {this.state.data.competition}
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <p>{this.state.data.text}</p>
